d0e8c64544c1a2a4acf47d04282d242a03771028
Верхозина Александра <aleksandraverhozina@yandex.ru>
2023-05-31 13:18:06 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -7,5 +7,17 @@ function showDate() {
     "Дата и время для испанской локали: "+ today.toLocaleString('es-EC') + '<br>' +
     "Дата и время для итальянской локали: "+ today.toLocaleString('it-IT') + '<br>';
     out.style.color = 'navy';
-    
 }
+function showDaysCount () {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    document.getElementById('dr').innerHTML = 'Количество дней с даты рождения: '+ daysCount;
+    inputDate.style.color = 'red';
+}
+function yd() {
+    document.getElementById('d').value = "";
+    document.getElementById('dr').innerHTML = "";
+  }
